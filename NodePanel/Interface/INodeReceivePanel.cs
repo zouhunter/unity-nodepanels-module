0578d270b5847631f89dae2e1296ed6a360b6f92
zouhangte <zouhangtezbm@126.com>
2017-05-27 14:29:11 +0800

    renew demo wait next upload

---
+++ NodePanel/Interface/INodeReceivePanel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+namespace PanelNode
+{
+    public interface INodeReceivePanel
+    {
+        bool CanOpen();
+        void HandleOpenData(object data);
+        bool CloseAble();
+    }
+}
---
+++ NodePanel/Interface/INodeSendPanel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+namespace PanelNode
+{
+    public interface INodeSendPanel
+    {
+        event UnityAction<string,UnityAction, object> OpenEvent;
+    }
+}
